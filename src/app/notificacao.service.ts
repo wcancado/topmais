bb43574dc0ed83d13260a3d3c19e7999598dea58
wcancado <wcancado@gmail.com>
2022-10-23 13:26:07 -0300

    build

---
+++ src/app/notificacao.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import {MatSnackBar} from '@angular/material/snack-bar';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NotificacaoService {
+
+  constructor(
+    private snackBar: MatSnackBar
+
+  ) { }
+  notificar(message: string) {
+    this.snackBar.open(message, "OK",{
+      duration: 2000,
+      verticalPosition: "top",
+      horizontalPosition:"center"
+    });
+  }
+}
